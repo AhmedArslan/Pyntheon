90abbc0d0d72e9e1cfe0f0aa5b6c68cd0fb79db1
AhmedArslan <ahmed.arslan@kuleuven.be>
2016-12-06 22:48:07 +0100

    Add files via upload

---
+++ tests/tests.py
@@ -0,0 +1,10 @@
+import unittest
+from pyntheon import unimap
+
+class PimpMyPyntheon(unittest.TestCase):
+	
+	def test_unimap(self):
+		self.assertTrue(True)
+
+if __name__ == '__main__':
+	unittest.main()
