93767e3acbada7dff762d01d3e916ebfee8cdfd5
Ahmed Arslan <aarslan@stanford.edu>
2019-08-04 01:45:56 -0700

    Update pyntheon.py

--- pyntheon/pyntheon.py
+++ pyntheon/pyntheon.py
@@ -195,7 +195,7 @@ def unimap(fname, species, ptm=None, mutation=None):
 				line = line.split()
 				with open(fname, 'rU') as user:
 					nam = user.name
-					n = nam.split('\')
+					n = nam.split('/')
 					n1 = na[-1].split('.')
 					for e in user:
 						e = e.split()
